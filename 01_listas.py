8aa32fc96cf38b162de86cad8e19dce04d7f9fc6
edmundo8avgz <67813513+edmundo8avgz@users.noreply.github.com>
2020-11-21 13:25:53 -0600

    agrego archivo para explicar listas

---
+++ 01_listas.py
@@ -0,0 +1,16 @@
+#listas
+
+numeros = [23,89,127,1289,8912]
+
+#para agregar elementos
+
+numero_extra = 33
+numero_extra_dos = 1223
+numero_extra_tres = 89
+
+numeros.append(numero_extra)
+print(numeros)
+
+#para elimniar el ultimo elemento usamos pop
+numeros.pop()
+print(numeros)
